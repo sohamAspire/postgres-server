051ba52f87933fccfa298c39073c560bd451ccd0
sohamAspire <soham.patel@aspiresoftserv.in>
2024-03-11 16:28:48 +0530

    fix : url string fixed and port

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.POOL_URL, {
+const db = new Sequelize(process.env.POOL_URL || 'postgres://default:6MvXL0cEAtnm@ep-morning-cloud-a1mxm0yw-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require', {
     logging: false,
     dialect: 'postgres'
 });
