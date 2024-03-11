2c8225acca20a6dcda47d70dd3659e6f6c23e6a8
sohamAspire <soham.patel@aspiresoftserv.in>
2024-03-11 16:25:07 +0530

    feat : Changed Connection String

--- db.js
+++ db.js
@@ -1,23 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('demo', 'postgres', 'spatel@012023', {
-    host: 'localhost',
-    logging : false,
+const db = new Sequelize(process.env.POOL_URL, {
+    logging: false,
     dialect: 'postgres'
 });
 
 
 module.exports = db
 
-
-// const Pool = require('pg').Pool
-
-// const Coonection_Configuration = new Pool({
-//     user : 'postgres',
-//     host : 'localhost',
-//     database : 'demo',
-//     password : 'spatel@012023',
-//     port : 5432
-// })
-
-// module.exports = Coonection_Configuration
